1c3b19335e80c466d0e7cbcb2d0280f3248c4eaf
Rasmus-Emil Hansen <r_emil@live.dk>
2021-02-25 20:07:06 +0100

    first commit

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::middleware('api')->get('/', function(Request $request) {
+Route::middleware('auth:api')->get('/', function(Request $request) {
     dd(phpinfo());
 });
